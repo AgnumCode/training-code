6b000cb94a81b7d8214b1dfb1de269293067b34f
Keegan O'Hara <keeganohara7@gmail.com>
2018-06-26 15:23:04 -0400

    First commit of FizzBuzz Program, looks good.

---
+++ Submissions/FizzBuzz/oharaFizzBuzz/oharaFizzBuzz.Console/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace oharaFizzBuzz
+{
+    class Program
+    {
+
+
+        public void FizzBuzz()
+        {
+            int Fizzes = 0;
+            int Buzzes = 0;
+            int fizzbuzzes = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    System.Console.WriteLine("fizzbuzz");
+                    fizzbuzzes++;
+                }
+                else if (i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                    Fizzes++;
+                }
+                else if (i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                    Buzzes++;
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+
+                
+            }
+
+            System.Console.WriteLine("# of Fizzes : " + Fizzes + System.Environment.NewLine + "# of Buzzes : " + Buzzes +
+                                     System.Environment.NewLine + "# of fizzbuzzes : " + fizzbuzzes);
+
+
+            /*
+             * 
+             * ADD Counters for Fizz's, Buzz's, and fizzbuzz's
+             * 
+             * */
+            int debugInt = 0;
+        }
+    
+
+
+        static void Main(string[] args)
+        {
+            Program keegansFizzBuzz = new Program();
+            keegansFizzBuzz.FizzBuzz();   
+        }
+
+        
+    }
+
+}
+   
+
