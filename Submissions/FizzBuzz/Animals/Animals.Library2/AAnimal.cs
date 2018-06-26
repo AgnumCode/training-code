e3384624794c8e69ab2675d085c8fa2ae8089d31
Angelo Walsh <angelo.walsh@me.com>
2018-06-26 19:54:34 -0400

    Removed Animals project from Fizzbuzz assignment folder

---
+++ Submissions/FizzBuzz/Animals/Animals.Library2/AAnimal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Library2
+{
+    public abstract class AAnimal : IAnimal
+    {
+        public string Name { get; set; }
+
+        public object GoAction { get; private set; }
+
+        public void GoToLocation(string location)
+        {
+            Console.WriteLine($"{GoAction} to {location}");
+        }
+
+        public abstract void MakeSound();
+       
+    }
+}
---
+++ Submissions/FizzBuzz/Animals/Animals.Library2/Bird.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Library2
+{
+    public class Bird : AAnimal
+    {
+        
+        public override string Name { get; set; }
+
+        public override string GoAction { get; set; } = "Flying";
+        {
+            Console.WriteLine($"Flying to {location}");
+        }
+
+        public void MakeSound();
+       
+    }
+}
