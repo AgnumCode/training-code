8702de7db5cade8174d6ebe84ae9d9de654a5b84
NRChowdhoury <nrchowdhoury@gmail.com>
2018-07-02 11:36:32 -0400

    Palindrome files

---
+++ Submissions/Palindrome/Palindrome/Palindrome/Palindrome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome.Library
+{
+    class Palindrome
+    {
+        public static bool CheckPalindrome(string str)
+        {
+            str = "abc cba";
+            int maxLength = str.Length - 1;
+            bool compare = false;
+
+            for (int i = 0; i < (str.Length/2); i++)
+            {
+                char forward = str[i];
+                char backward = str[maxLength];
+                maxLength--;
+
+                if (forward == backward)
+                {
+                    compare = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("This string is not a palindrome");
+                }
+
+
+            }
+
+            if (compare == true)
+            {
+                Console.WriteLine("This is a palindrome");
+            }
+        }
+    }
+}
+
+
+
---
+++ Submissions/Palindrome/Palindrome/PalindromeTest/PalindromeTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace PalindromeTest
+{
+    public class PalindromeTest
+    {
+        [Fact]
+        public void ShouldNotThrowException()
+        {
+            //Arrange
+            
+            //Act
+
+            //Assert
+            
+        }
+    }
+}
