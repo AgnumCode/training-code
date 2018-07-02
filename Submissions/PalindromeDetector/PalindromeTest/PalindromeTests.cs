1660671c0d473323c3414619011fa753bde150b4
Keegan O'Hara <keeganohara7@gmail.com>
2018-07-02 11:02:46 -0400

    First commit with PalindromeDetector class and one unit test

---
+++ Submissions/PalindromeDetector/PalindromeTest/PalindromeTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeDetector.Library;
+
+
+namespace PalindromeTest
+{
+    [TestClass]
+    public class PalindromeTests
+    {
+        [TestMethod]
+        public void TestCorrectPalindromeDetection()
+        {
+            string value = "racecar";
+            bool expected = true;
+            bool actual;
+
+            PalindromeDetector detector = new PalindromeDetector();
+            actual = detector.IsPalindrome(value);
+
+
+
+        }
+    }
+}
