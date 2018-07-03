054b4518f37b6500a0e5cd18f9278d64e102012a
RoyMamba0329 <roy.mamba0329@gmail.com>
2018-07-03 16:19:54 -0400

    palindrome fixed this is a late entry

--- Submissions/RBPalindrome/RBPalindrome.Test/PalindromeTest.cs
+++ Submissions/RBPalindrome/RBPalindrome.Test/PalindromeTest.cs
@@ -16,26 +16,49 @@ namespace RBPalindrome.Test
 
         }
 
-        public static IEnumerable<object[]> GetTestData()
-        {
-            yield return new object[] { new string[] { "Racecar", "121" }};
-            yield return new object[] { new string[] { "one two onw", "123abccba123" } };
-            yield return new object[] { new string[] { "Racecar", "nurses run", "1221", "never odd, or even" } };
-
-        }
-
         [Theory]
-        [MemberData(nameof(GetTestData))]
-        public void Palindromecheck(string[] data)
+        [InlineData(true, new string[] { "nurses run", "racecaR", "1221", "never odd, or even."})]
+        [InlineData(false, new string[] { "one two one", "123abccba123"})]
+
+        public void PalindromeCheck(bool expected, string[] data)
         {
+            // Arrange
             var pal = new Palindrome();
             pal.AddMany(data);
 
+            // Act
+
             foreach (string value in data)
             {
-                pal.IsPalindrome(value);
+              var actual = pal.IsPalindrome(value);
+
+              Assert.Equal(expected, actual);
             }
+
+            // Assert
+            
         }
+
+        //public static IEnumerable<object[]> GetTestData()
+        //{
+        //    yield return new object[] { new string[] { "Racecar", "121" }};
+        //    yield return new object[] { new string[] { "one two onw", "123abccba123" } };
+        //    yield return new object[] { new string[] { "Racecar", "nurses run", "1221", "never odd, or even" } };
+
+        //}
+
+        //[Theory]
+        //[MemberData(nameof(GetTestData))]
+        //public void Palindromecheck(string[] data)
+        //{
+        //    var pal = new Palindrome();
+        //    pal.AddMany(data);
+
+        //    foreach (string value in data)
+        //    {
+        //        pal.IsPalindrome(value);
+        //    }
+        //}
 
     }
 }
