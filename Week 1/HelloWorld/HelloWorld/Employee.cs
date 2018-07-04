33767ca2770f64c463d43a17079c232f323beed9
Rangeley Jay Delphin <Rangeleydelphin@gmail.com>
2018-07-04 12:24:59 -0400

    Adding HelloWorld with some C# sample codes in there

---
+++ Week 1/HelloWorld/HelloWorld/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    public class Employee
+    {
+        // uses default parameter-less constructor
+        public Employee()
+        {
+            name = "Jay";
+        }
+
+        // fields
+        public string name;
+        public decimal yearlySalary = 45000;
+
+        // methods
+        public decimal PaycheckAmount(decimal weeks)
+        {
+            return yearlySalary * (weeks / 52);
+        }
+    }
+}
